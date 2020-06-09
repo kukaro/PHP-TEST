adeaeb7504515c9c7569d52073e46a1b4d13353e
kukaro <justkukaro@naver.com>
2020-06-09 15:04:34 +0900

    add soft link

--- myubuntu/test/init.sh
+++ myubuntu/test/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed -ri 's/\/var\/www\/html/\/var\/www\/html\/public/g' /etc/apache2/sites-available/000-default.conf
+sed -ri 's/\/var\/www\/html/\/var\/www\/html\/page\/public/g' /etc/apache2/sites-available/000-default.conf
 
 /usr/sbin/apache2ctl -D FOREGROUND
 /usr/sbin/sshd -D FOREGROUND
